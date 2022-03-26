4b92e2f3a021a8dae7e3ed2421ee6612e080f3be
lah1ru <96978360+lah1ru@users.noreply.github.com>
2022-03-26 19:08:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,33 +25,41 @@ def main():
     else:
         p1=1
         
-    p2 = st.slider("Enter Your Age catogory",0,10)
+    p2 =st.number_input("Enter Your body temperature")
     
-    p3 =st.number_input("Enter Your body temperature")
+    p3 =st.number_input("Enter Your body puls rate")
+    #p3 = st.slider("Enter Your body puls rate",0,200)
     
-    #p4 =st.number_input("Enter Your body puls rate")
-    p4 = st.slider("Enter Your body puls rate",0,200)
+    #p4 = st.slider("Enter Your Respiration",0,50)
+    p4 = st.number_input("Enter Your Respiration")
     
-    #p5 =st.number_input("Enter Your body systolic blood pressure")
-    p5 = st.slider("Enter Your body systolic blood pressure",0,200)
+    p5 =st.number_input("Enter Your body systolic blood pressure")
+    #p5 = st.slider("Enter Your body systolic blood pressure",0,200)
     
-    #p6 =st.number_input("Enter Your body diastolic blood pressure")
-    p6 = st.slider("Enter Your body diastolic blood pressure",0,200)
+    p6 =st.number_input("Enter Your body diastolic blood pressure")
+    #p6 = st.slider("Enter Your body diastolic blood pressure",0,200)
     
-    p7 =st.number_input("Enter Your body weight	")
+    p7 =st.number_input("Enter Your body weight (kg)")
     
-    p8 =st.number_input("Enter Your body height	")
+    p8 = st.slider("Enter Your body height (cm)", 1,300)
+  
+    #p9 =st.number_input("Enter Your BMI Value")
+    bmi = (p7/p8**2)*10000
+    p9 = format(bmi, '.2f')
+    st.metric(label="your BMI:", value=p9, delta_color="red")
+
     
-    p9 =st.number_input("Enter Your BMI Value")
+    p10 = st.slider("Enter Your Age catogory",0,10)
     
     s2=st.selectbox("Do you have Myocardial infarction",("Yes","No"))
     if s2=="Yes":
-        p10=1
+        p11=1
     else:
-        p10=0
+        p11=0
+         
     
     if st.button('Predict'):
-       prediction = model.predict([[p1,p2,p3,p4,p5,p6,p7,p8,p9,p10]])
+       prediction = model.predict([[p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11]])
        
        st.success('HF Type: {} '.format(prediction[0]))   
         
